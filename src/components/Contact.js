6906f7676eff9b4e24688d7a60f0d27ee0696a7d
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2020-01-04 12:54:58 -0500

    More experimenting with SCSS and Psuedo elements. Also working with caret-color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <main>
+      <div>Contact</div>
+      <form>
+        <input
+          type="email"
+          className="inputfield input1"
+          placeholder="email"
+        ></input>
+        <input
+          type="password"
+          className="inputfield input2"
+          placeholder="password"
+        ></input>
+        <input
+          type="text"
+          name="pin"
+          maxlength="4"
+          size="4"
+          className="inputfield input3"
+          placeholder="pin"
+        ></input>
+        <textarea
+          rows="5"
+          cols="50"
+          className="inputfield input4"
+          placeholder="Post!...."
+        ></textarea>
+      </form>
+    </main>
+  );
+};
 
 export default Contact;

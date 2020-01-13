88ecce174257df66544e0adcee0c481f1021a260
RicoVendritto <54893598+RicoVendritto@users.noreply.github.com>
2020-01-13 12:57:15 -0500

    First Letter + First Line psuedo element

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { logRoles } from "@testing-library/react";
 
 const About = () => {
   return (
     <main>
-      <blockquote>The greatest obstacle to discovery is not ignorance, but the illusion of knowledge. - Daniel Boorstin</blockquote>
+      <blockquote>
+        The greatest obstacle to discovery is not ignorance, but the illusion of
+        knowledge. - Daniel Boorstin
+      </blockquote>
+      <br />
+      <div>
+        <p className="loremP">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p className="loremP">
+          Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia
+          deserunt mollit anim id est laborum.
+        </p>
+        <p className="loremPline">
+          Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </p>
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default About;
